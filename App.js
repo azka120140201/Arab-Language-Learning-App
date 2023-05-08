22791375b13d1f6ef7ae15206ee8cd234473d893
azka120140201 <azka.120140201@student.itera.ac.id>
2023-05-08 19:28:49 +0700

    Perbaikan App.js

--- App.js
+++ App.js
@@ -11,12 +11,9 @@ import splashScreen from './screen/splashScreen';
 import menuKuis from './screen/menuKuis';
 import home from './screen/home';
 import materi from './screen/materi';
-<<<<<<< HEAD
+import isiMateri from './screen/isiMateri';
 import profil from './screen/profil';
 
-=======
-import isiMateri from './screen/isiMateri';
->>>>>>> ce40d71adda6a768c24b0645d506f034d0df7d97
 const Stack = createNativeStackNavigator();
 
 function App() {
@@ -32,11 +29,7 @@ function App() {
         <Stack.Screen name="isiMateri" component={isiMateri} />
         <Stack.Screen name="menuKuis" component={menuKuis} />
         <Stack.Screen name="home" component={home} />
-<<<<<<< HEAD
         <Stack.Screen name="profil" component={profil} />
-=======
-        
->>>>>>> ce40d71adda6a768c24b0645d506f034d0df7d97
       </Stack.Navigator>
     </NavigationContainer>
   );
