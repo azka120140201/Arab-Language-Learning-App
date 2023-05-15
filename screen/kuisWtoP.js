1073aa60d5cf6b62c02071291bc6a9c8e5f5cb31
novianagresita <130279889+novianagresita@users.noreply.github.com>
2023-05-15 11:15:27 +0700

    tombol mulai dan score kuis : WtoP

--- screen/kuisWtoP.js
+++ screen/kuisWtoP.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Alert, Button, StyleSheet, View, Text, TouchableOpacity, Image } from "react-native";
-import ProgressBar from "../assets/ProgressBar";
+import ProgressBar from "./assets/ProgressBar";
 
 export default class Quiz extends Component {
   constructor(props) {
@@ -36,6 +36,8 @@ export default class Quiz extends Component {
           correctAnswer: 0,
         },
       ],
+      score: 0, // Skor awal
+      quizFinished: false, // Apakah kuis sudah selesai atau belum
     };
   }
 
@@ -44,7 +46,32 @@ export default class Quiz extends Component {
   }
 
   startQuiz() {
-    this.setState({ quizStarted: true }, () => {
+      this.setState({ quizStarted: true }, () => {
+        this.interval = setInterval(() => {
+          if (this.state.timeRemaining > 0) {
+            this.setState({ timeRemaining: this.state.timeRemaining - 1 });
+          } else {
+            this.nextQuestion();
+          }
+        }, 1000);
+      });
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.interval);
+    }
+  
+    nextQuestion() {
+      if (this.state.currentQuestion === this.state.questions.length - 1) {
+        Alert.alert("Quiz is finished!");
+        this.setState({ quizFinished: true });
+      } else {
+        this.setState({
+          currentQuestion: this.state.currentQuestion + 1,
+          timeRemaining: 60,
+        });
+      }
+      clearInterval(this.interval);
       this.interval = setInterval(() => {
         if (this.state.timeRemaining > 0) {
           this.setState({ timeRemaining: this.state.timeRemaining - 1 });
@@ -52,91 +79,84 @@ export default class Quiz extends Component {
           this.nextQuestion();
         }
       }, 1000);
-    });
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  nextQuestion() {
-    if (this.state.currentQuestion === this.state.questions.length - 1) {
-      Alert.alert("Quiz is finished!");
-      this.setState({ currentQuestion: 0, timeRemaining: 60 });
-    } else {
-      this.setState({
-        currentQuestion: this.state.currentQuestion + 1,
-        timeRemaining: 60,
-      });
     }
-    clearInterval(this.interval);
-    this.interval = setInterval(() => {
-      if (this.state.timeRemaining > 0) {
-        this.setState({ timeRemaining: this.state.timeRemaining - 1 });
+  
+    handleAnswer(selectedIndex) {
+      const currentQuestion = this.state.questions[this.state.currentQuestion];
+      clearInterval(this.interval);
+      if (selectedIndex === currentQuestion.correctAnswer) {
+        // Menambahkan 1 pada skor jika jawaban benar
+        this.setState({ score: this.state.score + 1 });
+        Alert.alert("Correct!");
       } else {
-        this.nextQuestion();
+        Alert.alert("Incorrect!");
       }
-    }, 1000);
-  }
-
-  render() {
-  const currentQuestion = this.state.questions[this.state.currentQuestion];
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.questionNumber}>
-          Question {this.state.currentQuestion + 1}
-        </Text>
-        <ProgressBar
-          progress={0.5}
-          width={300}
-          height={8}
-          color={"#93C572"}
-          secondColor={"#FFE5D9"}
-        />
-      </View>
-      <Text style={styles.question}>{currentQuestion.question}</Text>
-      <View style={styles.row}>
-        <TouchableOpacity onPress={() => this.handleAnswer(0)}>
-          <View style={styles.column}>
-            <Image source={{ uri: currentQuestion.options[0] }} style={styles.image} />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => this.handleAnswer(1)}>
-          <View style={styles.column}>
-            <Image source={{ uri: currentQuestion.options[1] }} style={styles.image} />
-          </View>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.row}>
-        <TouchableOpacity onPress={() => this.handleAnswer(2)}>
-          <View style={styles.column}>
-            <Image source={{ uri: currentQuestion.options[2] }} style={styles.image} />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => this.handleAnswer(3)}>
-          <View style={styles.column}>
-            <Image source={{ uri: currentQuestion.options[3] }} style={styles.image} />
-          </View>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-}
-
-
-
-  handleAnswer(selectedIndex) {
-    const currentQuestion = this.state.questions[this.state.currentQuestion];
-    clearInterval(this.interval);
-    if (selectedIndex === currentQuestion.correctAnswer) {
-      Alert.alert("Correct!");
-    } else {
-      Alert.alert("Incorrect!");
+      this.nextQuestion();
     }
-    this.nextQuestion();
+  
+    render() {
+      const currentQuestion = this.state.questions[this.state.currentQuestion];
+      return (
+        <View style={styles.container}>
+          {!this.state.quizStarted && !this.state.quizFinished && (
+            <TouchableOpacity style={styles.startButton} onPress={() => this.startQuiz()}>
+              <Text style={styles.startButtonText}>Start Quiz</Text>
+            </TouchableOpacity>
+          )}
+  
+          {this.state.quizStarted && !this.state.quizFinished && (
+            <View>
+              <View style={styles.header}>
+                <Text style={styles.questionNumber}>
+                  Question {this.state.currentQuestion + 1}
+                </Text>
+                <ProgressBar
+                  progress={0.5}
+                  width={300}
+                  height={8}
+                  color={"#93C572"}
+                  secondColor={"#FFE5D9"}
+                />
+              </View>
+  
+              <Text style={styles.question}>{currentQuestion.question}</Text>
+              <View style={styles.row}>
+                <TouchableOpacity onPress={() => this.handleAnswer(0)}>
+                  <View style={styles.column}>
+                    <Image source={{ uri: currentQuestion.options[0] }} style={styles.image} />
+                  </View>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => this.handleAnswer(1)}>
+                  <View style={styles.column}>
+                    <Image source={{ uri: currentQuestion.options[1] }} style={styles.image} />
+                  </View>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.row}>
+              <TouchableOpacity onPress={() => this.handleAnswer(2)}>
+                <View style={styles.column}>
+                  <Image source={{ uri: currentQuestion.options[2] }} style={styles.image} />
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => this.handleAnswer(3)}>
+                <View style={styles.column}>
+                  <Image source={{ uri: currentQuestion.options[3] }} style={styles.image} />
+                </View>
+              </TouchableOpacity>
+            </View>
+            </View>
+          )}
+  
+          {this.state.quizFinished && (
+            <View style={styles.scoreContainer}>
+              <Text style={styles.scoreText}>Quiz Finished!</Text>
+              <Text style={styles.scoreText}>Your Score: {this.state.score}</Text>
+            </View>
+          )}
+        </View>
+      );
+    }
   }
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -148,7 +168,7 @@ const styles = StyleSheet.create({
   question: {
     fontSize: 20,
     fontWeight: "bold",
-    marginTop: 50,
+    marginTop: 100,
     marginBottom: 20,
     textAlign: "center",
     fontFamily: "SpaceGrotesk",
@@ -189,5 +209,28 @@ const styles = StyleSheet.create({
     height: 100,
     borderRadius: 15,
     resizeMode: "cover",
+  },
+  startButton: {
+    backgroundColor: "#93C572",
+    padding: 12,
+    borderRadius: 10,
+  },
+  startButtonText: {
+    color: "#FFFFFF",
+    fontFamily: "SpaceGrotesk",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  scoreContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  scoreText: {
+    fontFamily: "SpaceGrotesk",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
   },
 });
