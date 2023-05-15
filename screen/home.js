0f7b7e2ff40e51d1855da7958ccb93e605186936
Nurul Fikri <nurulfikri756@gmail.com>
2023-05-16 01:37:48 +0700

    memperbaharui tampilan home

--- screen/home.js
+++ screen/home.js
@@ -1,31 +1,37 @@
-import React, { Component, useEffect, useState } from 'react';
-import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, TextInput, TouchableOpacity, ActivityIndicator} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import { useFonts } from 'expo-font';
 
 
-const home = ({ navigation }) => {
+const login = ({ navigation }) => {
+     const [loaded] = useFonts({
+          SpaceGrotesk: require('../assets/fonts/SpaceGrotesk-VariableFont_wght.ttf'),
+        });
+
+     if (!loaded) {
+     return null;
+     }
 
      return (
           <View
                style={{
                     flex: 1,
                     backgroundColor: '#F9F6EE',
-               }}
-          >
+                    
+               }}>
                <View
                     style={{
-                         flex: 0.9,
+                         justifyContent: 'center',
                          alignItems: 'center',
-                         marginTop: 0,
-                    }}
-               >
+                         marginTop: 50,
+                    }}>
                     <Image
                          source={require('../src/images/Logo.png')}
                          style={{
                               width: 150,
                               height: 150,
-
-                              marginTop: 60,
+                              marginBottom: -20,
 
                               shadowColor: 'black',
                               shadowOffset: { width: 0, height: 2 },
@@ -33,68 +39,193 @@ const home = ({ navigation }) => {
                               shadowRadius: 4,
                          }} />
 
-                    <Text style={{ fontSize: 24, fontWeight: 'bold', }}>DARSU<Text style={{ color: '#72A152' }}>ARAB</Text></Text>
-
+                    <Text style={{ fontSize: 24, fontWeight: 'bold',fontFamily: 'SpaceGrotesk' }}>DARSU<Text style={{ color: '#72A152' }}>ARAB</Text></Text>
+                   
                </View>
 
-               <View
-                    style={{
-                         flex: 0.1,
-                         backgroundColor: '#93C572',
-                         borderTopLeftRadius: 15,
-                         borderTopRightRadius: 15
+
+              <TouchableOpacity  onPress={() => navigation.navigate('materi')}>
+                    <View style={{
+                         flexDirection: 'row',
+                         marginHorizontal: 20,
+                         marginTop: 20,
                     }}>
+                         <View
+                              style={{
+                                   justifyContent: 'center',
+                                   alignItems: 'center',
+                                   backgroundColor: '#93C572',
+                                   width: 50,
+                                   borderTopLeftRadius: 5,
+                                   borderBottomLeftRadius: 5,
+                                   elevation: 10,
+                                   
 
-                    <View style={{ marginTop: 20, flexDirection: 'row' }}>
-                         <TouchableOpacity
-                              onPress={() => navigation.navigate('materi')}
+                                   shadowColor: 'black',
+                                   shadowOffset: { width: 0, height: 2 },
+                                   shadowOpacity: 0.5,
+                                   shadowRadius: 4,
+                              }}>
+                              <Icon name="book" size={17} color="#111" />
+                         </View>
+                         <Text
                               style={{
+                                   backgroundColor: '#93C572',
+                                   borderTopRightRadius: 5,
+                                   borderBottomRightRadius: 5,
                                    flex: 1,
-                                   justifyContent: 'center',
-                                   alignItems: 'center'
-                              }}
-                         >
-                              <Icon name="book" size={20} color="#111" />
-                         </TouchableOpacity>
-                         <TouchableOpacity
-                              onPress={() => navigation.navigate('menuKuis')}
-                              style={{
-                                   flex: 1,
-                                   justifyContent: 'center',
-                                   alignItems: 'center'
-                              }}
-                         >
-                              <Icon name="quidditch" size={20} color="#111" />
-                         </TouchableOpacity>
-                         <TouchableOpacity
-                              onPress={() => navigation.navigate('Leaderboard')}
-                              style={{
-                                   flex: 1,
-                                   justifyContent: 'center',
-                                   alignItems: 'center'
-                              }}
-                         >
-                              <Icon name="chart-bar" size={20} color="#111" />
-                         </TouchableOpacity>
-                         <TouchableOpacity
-                              onPress={() => navigation.navigate('profil')}
-                              style={{
-                                   flex: 1,
-                                   justifyContent: 'center',
-                                   alignItems: 'center'
-                              }}
-                         >
-                              <Icon name="user" size={20} color="#111" />
-                         </TouchableOpacity>
+                                   paddingVertical: 15,
+                                   elevation: 10,
+                                   paddingLeft: 10,
+                                   fontFamily: 'SpaceGrotesk',
+                                   fontWeight: "bold",
 
+                                   shadowColor: 'black',
+                                   shadowOffset: { width: 0, height: 2 },
+                                   shadowOpacity: 0.5,
+                                   shadowRadius: 4,
+                              }}>
+                              Lesson</Text>
                     </View>
-
-               </View>
-
+               </TouchableOpacity>
 
 
+              <TouchableOpacity  onPress={() => navigation.navigate('menuKuis')}>
+                    <View style={{
+                         flexDirection: 'row',
+                         marginHorizontal: 20,
+                         marginTop: 20,
+                    }}>
+                         <View
+                              style={{
+                                   justifyContent: 'center',
+                                   alignItems: 'center',
+                                   backgroundColor: '#93C572',
+                                   width: 50,
+                                   borderTopLeftRadius: 5,
+                                   borderBottomLeftRadius: 5,
+                                   elevation: 10,
+                                   
 
+                                   shadowColor: 'black',
+                                   shadowOffset: { width: 0, height: 2 },
+                                   shadowOpacity: 0.5,
+                                   shadowRadius: 4,
+                              }}>
+                              <Icon name="pen" size={17} color="#111" />
+                         </View>
+                         <Text
+                              style={{
+                                   backgroundColor: '#93C572',
+                                   borderTopRightRadius: 5,
+                                   borderBottomRightRadius: 5,
+                                   flex: 1,
+                                   paddingVertical: 15,
+                                   elevation: 10,
+                                   paddingLeft: 10,
+                                   fontFamily: 'SpaceGrotesk',
+                                   fontWeight: "bold",
+
+                                   shadowColor: 'black',
+                                   shadowOffset: { width: 0, height: 2 },
+                                   shadowOpacity: 0.5,
+                                   shadowRadius: 4,
+                              }}>
+                              Quis</Text>
+                    </View>
+               </TouchableOpacity>
+              <TouchableOpacity>
+                    <View style={{
+                         flexDirection: 'row',
+                         marginHorizontal: 20,
+                         marginTop: 20,
+                    }}>
+                         <View
+                              style={{
+                                   justifyContent: 'center',
+                                   alignItems: 'center',
+                                   backgroundColor: '#93C572',
+                                   width: 50,
+                                   borderTopLeftRadius: 5,
+                                   borderBottomLeftRadius: 5,
+                                   elevation: 10,
+                                   
+
+                                   shadowColor: 'black',
+                                   shadowOffset: { width: 0, height: 2 },
+                                   shadowOpacity: 0.5,
+                                   shadowRadius: 4,
+                              }}>
+                              <Icon name="chart-bar" size={17} color="#111" />
+                         </View>
+                         <Text
+                              style={{
+                                   backgroundColor: '#93C572',
+                                   borderTopRightRadius: 5,
+                                   borderBottomRightRadius: 5,
+                                   flex: 1,
+                                   paddingVertical: 15,
+                                   elevation: 10,
+                                   paddingLeft: 10,
+                                   fontFamily: 'SpaceGrotesk',
+                                   fontWeight: "bold",
+
+                                   shadowColor: 'black',
+                                   shadowOffset: { width: 0, height: 2 },
+                                   shadowOpacity: 0.5,
+                                   shadowRadius: 4,
+                              }}>
+                              Leaderboard</Text>
+                    </View>
+               </TouchableOpacity>
+              <TouchableOpacity>
+                    <View style={{
+                         flexDirection: 'row',
+                         marginHorizontal: 20,
+                         marginTop: 20,
+                    }}>
+                         <View
+                              style={{
+                                   justifyContent: 'center',
+                                   alignItems: 'center',
+                                   backgroundColor: '#93C572',
+                                   width: 50,
+                                   borderTopLeftRadius: 5,
+                                   borderBottomLeftRadius: 5,
+                                   elevation: 10,
+                                   
+
+                                   shadowColor: 'black',
+                                   shadowOffset: { width: 0, height: 2 },
+                                   shadowOpacity: 0.5,
+                                   shadowRadius: 4,
+                              }}>
+                              <Icon name="user" size={17} color="#111" />
+                         </View>
+                         <Text
+                              style={{
+                                   backgroundColor: '#93C572',
+                                   borderTopRightRadius: 5,
+                                   borderBottomRightRadius: 5,
+                                   flex: 1,
+                                   paddingVertical: 15,
+                                   elevation: 10,
+                                   paddingLeft: 10,
+                                   fontFamily: 'SpaceGrotesk',
+                                   fontWeight: "bold",
+
+                                   shadowColor: 'black',
+                                   shadowOffset: { width: 0, height: 2 },
+                                   shadowOpacity: 0.5,
+                                   shadowRadius: 4,
+                              }}>
+                              Profil</Text>
+                    </View>
+               </TouchableOpacity>
+
+
+             
           </View>
      );
 }
-export default home;
+export default login;
